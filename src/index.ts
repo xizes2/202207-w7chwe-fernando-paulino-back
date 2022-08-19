a62c41fa53c267f9ee302ed66de6c1f7b977ce7d
Fernando Paulino <xizes2@hotmail.com>
2022-08-19 20:39:23 +0200

    set up database and call function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import connectDatabase from "./database";
+import "./dotenv";
 
+const databaseUrl = process.env.MONGOURL;
+
+(async () => {
+  try {
+    await connectDatabase(databaseUrl);
+  } catch (error) {
+    process.exit(1);
+  }
+})();
