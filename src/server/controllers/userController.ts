4c1f52b7b1117a6828eb1e5757353ce2b59c7cc0
Fernando Paulino <xizes2@hotmail.com>
2022-08-21 21:36:07 +0200

    delete non used lines

--- src/server/controllers/userController.ts
+++ src/server/controllers/userController.ts
@@ -51,5 +51,3 @@ export const registerUser = async (
     next(customError);
   }
 };
-
-export const loginUser = () => {};
--- src/server/routers/usersRouter.ts
+++ src/server/routers/usersRouter.ts
@@ -1,14 +1,9 @@
 import Router from "express";
-import {
-  getUsers,
-  loginUser,
-  registerUser,
-} from "../controllers/userController";
+import { getUsers, registerUser } from "../controllers/userController";
 
 const usersRouter = Router();
 
 usersRouter.get("/", getUsers);
 usersRouter.post("/register", registerUser);
-usersRouter.post("/login", loginUser);
 
 export default usersRouter;
